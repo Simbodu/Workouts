8563539fbbf43d84cb9b80a9161e6c1167e7a5ac
Simbodu <simonboven@icloud.com>
2025-10-19 15:53:39 +0200

    Bugsss

--- Workoutstracker.py
+++ Workoutstracker.py
@@ -56,14 +56,11 @@ def login_user(username, password):
     st.session_state.logged_in = True
     st.session_state.username = username
     st.success(f"Logged in as {username}")
-    # Force rerun so UI updates immediately
-    st.set_query_params(_rerun=str(datetime.now()))
     return True
 
 def logout_user():
     st.session_state.logged_in = False
     st.session_state.username = ""
-    st.set_query_params(_rerun=str(datetime.now()))
 
 # --------- Login / Create Account ---------
 if not st.session_state.logged_in:
